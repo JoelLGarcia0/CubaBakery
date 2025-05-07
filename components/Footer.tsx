40bab1f0d0e6000ab7c1f41b7413d7cf28a67dae
Joel Garcia <157417367+JoelLGarcia0@users.noreply.github.com>
2025-05-07 18:46:15 -0400

    change footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="w-full bg-darkGrey py-4 table-footer-group">
-        <div className="flex items-center justify-center space-x-2">
-          <p className="font-titleFont text-offWhite text-sm text-center">
-          © Copyright 2025 Cuba Bakery, Inc. All Rights Reserved. This website was created and is managed by Joel Garcia.
-          </p>
-        </div>
+      <div className="flex items-center justify-center space-x-2">
+        <p className="font-titleFont text-offWhite text-sm text-center">
+          © Copyright 2025 Cuba Bakery, Inc. All Rights Reserved. This website
+          was created by{" "}
+          <a className="font-semibold" href="https://restweb.dev">
+            RESTWeb.dev
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
